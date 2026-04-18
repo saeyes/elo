8ad62e03eaf478f8f5e9c202adddb295986fcc44
saeyes <gim786827@gmail.com>
2026-04-18 14:22:05 +0900

    Increase player generation limit to 14999

--- matching_project/app.py
+++ matching_project/app.py
@@ -34,7 +34,7 @@ def index():
 
         my_skill = max(0, min(my_skill, 5000))
 
-        players = generate_players(59)
+        players = generate_players(14999)
         players.append({
             "id": "YOU",
             "skill": my_skill
